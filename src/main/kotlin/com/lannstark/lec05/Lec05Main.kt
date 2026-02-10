2e6e060e674d17e5f54eff9fac8452d15a32e973
llocr <36-96@naver.com>
2026-02-10 15:34:16 +0900

    5-1. 코틀린에서 제어문을 다루는 방법

---
+++ src/main/kotlin/com/lannstark/lec05/Lec05Main.kt
@@ -0,0 +1,75 @@
+package com.lannstark.lec05
+
+fun validateScoreIsNotNegative(score: Int) {
+//    if (score < 0) {
+//        throw IllegalArgumentException("${score}는 0보다 작을 . 없습니다.")
+//    }
+
+    if (score !in 0..100) {
+        throw IllegalArgumentException("score의 범위는 0부터 100까지입니다.")
+    }
+}
+
+fun getPassOrFail(score: Int) :String {
+//    if (score >= 50) {
+//        return "P"
+//    } else {
+//        return "F"
+//    }
+
+    return if (score >= 50) {
+        "P"
+    } else {
+        "F"
+    }
+}
+
+fun getGrade(score: Int): String {
+    return if (score >= 90) {
+        "A"
+    } else if(score >= 80) {
+        "B"
+    } else if (score >= 70) {
+        "C"
+    } else {
+        "D"
+    }
+}
+
+fun getGradeWithSwitch(score: Int): String {
+//    return when(score / 10) {
+//        9 -> "A"
+//        8 -> "B"
+//        7 -> "C"
+//        else -> "D"
+//    }
+
+    return when(score) {
+        in 90..99 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "D"
+    }
+}
+
+fun startsWithA(obj: Any): Boolean {
+    return when(obj) {
+        is String -> obj.startsWith("A")
+        else -> false
+    }
+}
+
+fun judgeNumber(number: Int) {
+    when (number) {
+        1, 0, -1 -> println("어디서 많이 본 숫자입니다.")
+        else -> println("1, 0, -1 이 아닙니다.")
+    }
+}
+
+fun judgeNumber2(number: Int) {
+    when {
+        number == 0 -> println("주어진 숫자는 0입니다.")
+        number % 2 == 0 -> println("주어진 숫자는 짝수입니다.")
+        else -> println("주어지는 숫자는 홀수입니다.")
+    }
+}
